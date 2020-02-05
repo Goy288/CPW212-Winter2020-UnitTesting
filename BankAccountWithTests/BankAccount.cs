040c0d86cd8eb4d6aec6a7d23f9e8fc28f8e3a86
Joseph Ortiz <joe@speakinginbits.com>
2020-02-05 14:50:08 -0800

    Add project files.

---
+++ BankAccountWithTests/BankAccount.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountWithTests
+{
+    public class BankAccount
+    {
+        private string accountNumber;
+
+        public BankAccount(string accNum)
+        {
+            AccountNumber = accNum;
+        }
+
+        public string AccountNumber 
+        {
+            get 
+            { 
+                return accountNumber; 
+            }
+            set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Account number cannot be null or whitespace");
+                }
+                accountNumber = value; 
+            }   
+        }
+
+        public string Owner { get; set; }
+
+        public double Balance { get; private set; }
+
+        /// <summary>
+        /// Deposits a positive amount of money
+        /// into the account and returns the new
+        /// <see cref="Balance"/>
+        /// </summary>
+        /// <exception cref="ArgumentException">
+        ///     Thrown when <paramref name="amt"/> is 0 or less
+        /// </exception>
+        /// <param name="amt">The amount of money to deposit</param>
+        public double Deposit(double amt)
+        {
+            if (amt <= 0)
+            {
+                throw new ArgumentException("You must deposit a positive amount");
+            }
+            Balance += amt;
+            return Balance;
+        }
+    }
+}
---
+++ BankAccountWithTests/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountWithTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var acc = new BankAccount(null);
+            //var acc = new BankAccount("");
+            //var acc = new BankAccount("      ");
+            //acc.AccountNumber = null;
+            // acc.Balance = 1000000;
+        }
+    }
+}
